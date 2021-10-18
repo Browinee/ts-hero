5308b33a1b33f41b214d5fe1082b3a992a5c576a
justin.lin <justin.lin@yxunistar.com>
2021-10-18 13:26:31 +0800

    feat: add createStore and define action

--- src/vuex/index.ts
+++ src/vuex/index.ts
@@ -5,6 +5,7 @@ interface Getter<T, U> {
 interface GetterTree<T, U> {
     [key: string]: Getter<T, U>;
 }
+
 interface Mutation<T> {
     (state: T, payload?: any): void
 }
@@ -13,14 +14,42 @@ interface MutationTree<T> {
     [key: string]: Mutation<S>
 }
 
+interface Dispatch {
+    (type: string, payload?: any): any
+}
+
+interface Commit {
+    (type: string, payload?: any): any
+}
+
+interface ActionContext<T, U> {
+    dispatch: Dispatch;
+    commit: Commit;
+    state: T;
+}
+
+interface Actions<T, U> {
+    (context: ActionContext<T, U>, payload: any): void
+}
+
+interface ActionTree<T, U> {
+    [key: string]: Actions<T, U>;
+}
+
 interface StoreOptions<T> {
     state: T;
     getters: GetterTree<T, T>;
     mutations: MutationTree<T>;
+    actions: ActionTree<T, T>;
 
 }
 
-class Store<S = any>{
-    constructor(options: StoreOptions<S>){
+
+function createStore<S> (options: StoreOptions<S>) {
+    return new Store<S>(options);
+}
+
+class Store<S = any> {
+    constructor (options: StoreOptions<S>) {
     }
 }
